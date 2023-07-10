7d8e01e11dca5c4f0f42f09f6af85d62ffd01fa0
Diego Gonzalez <diegonzalezv93@gmail.com>
2023-07-10 00:00:42 -0300

    nueva unidad vectores

---
+++ unidad7Vectores/ejemplosVectorDouble/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+//vecores double
+
+
+double[] muestraMatutina= new double[31];
+double[] muestraVespertina= new double[31];
+double[] muestraNocturna= new double[31];
+
+int i;
+
+for (i=0;i<31;i++)
+{
+    Console.WriteLine("Ingrese la muestra matutina del dia #",i+1);
+    muestraMatutina[i]=double.Parse(Console.ReadLine());
+    Console.WriteLine("Ingrese la muestra vespertina del dia #",i+1);
+    muestraVespertina[i]=double.Parse(Console.ReadLine());
+    Console.WriteLine("Ingrese la muestra nocturna del dia #",i+1);
+    muestraNocturna[i]=double.Parse(Console.ReadLine());
+}
---
+++ unidad7Vectores/ejemplosVectores/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+// vectores arrays arreglos 
+
+int[] vectorNumeros = new int[10]; 
+
+int i;
+
+for (i = 0; i < 10; i++)
+{
+    Console.WriteLine("Ingrese un numero");
+    vectorNumeros[i] = int.Parse(Console.ReadLine());
+}
+
+for(i=0;i<10;i++)
+{
+    Console.WriteLine(vectorNumeros[i]);
+}
