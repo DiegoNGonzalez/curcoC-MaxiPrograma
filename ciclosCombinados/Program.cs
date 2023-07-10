d073d296af10264178d866c3c6cb635ad2cfe13f
Diego Gonzalez <diegonzalezv93@gmail.com>
2023-07-09 21:38:49 -0300

    ejercicios ciclos combinados

---
+++ ciclosCombinados/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+int prom, acu =0, n;
+
+
+for (int i = 0; i < 20; i++)
+{
+    Console.WriteLine("Ingrese la edad: ");
+    n = int.Parse(Console.ReadLine());
+    acu +=n;
+}
+prom = acu / 20;
+Console.WriteLine("El promedio de edad es: " + prom);
---
+++ ejercicios6-1/Program.cs
@@ -0,0 +1,22 @@
+﻿//1. Hacer un programa para ingresar 10 números. El mismo debe analizar y mostrar por pantalla cuántos de esos números son primos.
+
+int i, numero, contador=0, j, primos=0;
+
+for (i=1; i<=10; i++)
+{
+    Console.WriteLine("Ingrese un numero");
+    numero = int.Parse(Console.ReadLine());
+    contador = 0;
+    for (j=1; j<=numero; j++)
+    {
+        if (numero % j == 0)
+        {
+            contador++;
+        }
+    }
+    if (contador == 2)
+    {
+        primos++;
+    }
+}
+Console.WriteLine("La cantidad de numeros primos es: " + primos);
