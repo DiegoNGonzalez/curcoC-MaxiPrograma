ed5fb7069deae7935aa75f52535450753c51936e
Diego Gonzalez <diegonzalezv93@gmail.com>
2023-07-09 22:36:31 -0300

    resolucion ejercicio 3

---
+++ ejercicios6-3/Program.cs
@@ -0,0 +1,18 @@
+﻿//  Hacer un programa para recibir listas de números positivos que están separadas entre sí por un cero. El fin de la carga se notifica con un número negativo. Luego mostrar cuántos números tiene cada lista.
+
+int numero, contadorLista=0, contadorNumeros=0;
+
+Console.WriteLine("Ingrese un numero");
+numero = int.Parse(Console.ReadLine());
+while(numero >= 0){
+    while(numero != 0){
+        contadorNumeros++;
+        Console.WriteLine("Ingrese un numero");
+        numero = int.Parse(Console.ReadLine());
+    }
+    contadorLista++;
+    Console.WriteLine("La cantidad de numeros de la lista #" + contadorLista + " es: " + contadorNumeros);
+    contadorNumeros = 0;
+    Console.WriteLine("Ingrese un numero");
+    numero = int.Parse(Console.ReadLine());
+}
